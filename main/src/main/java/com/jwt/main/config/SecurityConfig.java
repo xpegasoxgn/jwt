d6e7b224ed83fff4bf0fe3d44bbf7dfbb1cb00aa
gnavia <ginavia12@gmail.com>
2026-03-23 21:32:50 -0400

    jwt prueba end point

--- main/src/main/java/com/jwt/main/config/SecurityConfig.java
+++ main/src/main/java/com/jwt/main/config/SecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
 import com.jwt.main.security.JwtAuthenticationFilter;
@@ -19,6 +20,7 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http
                 .csrf(csrf -> csrf.disable())
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests( auth -> auth
                     .requestMatchers("/auth/login").permitAll()
                     .anyRequest().authenticated()
